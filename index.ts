06fa5e5216f26ccac57e820bbd5d32b0ce2ccec5
unknown <denis991280@gmail.com>
2025-06-04 09:11:00 +0200

    step1

---
+++ index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import "./db";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
