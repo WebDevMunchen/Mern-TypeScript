ac961029c005a96d89cf1b35037452199c76a22e
unknown <denis991280@gmail.com>
2025-06-10 15:21:24 +0200

    step9

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,7 +1,30 @@
+import { useForm, type SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+
 export default function Register() {
-    return(
-        <h1>
-            Register
-        </h1>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" placeholder="max.musterman@net.com"{...register("email", { required: true })} />
+      {errors.email && <span>This field is required</span>}
+
+      <input type="password" placeholder="******" {...register("password", {required: true})} />
+      {errors.password && <span>This field is required</span>}
+
+
+      <input type="submit" />
+    </form>
+  )
 }
